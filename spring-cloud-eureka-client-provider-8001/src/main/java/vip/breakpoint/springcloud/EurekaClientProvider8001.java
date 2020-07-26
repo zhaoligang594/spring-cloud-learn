6671a7f68cd064fc45b1568b0a6b3dd0d284b9d6
赵立刚 <zlgtop@163.com>
2020-07-26 09:03:39 +0800

    Consul 服务注册中心的安装以及配置

--- spring-cloud-eureka-client-provider-8001/src/main/java/vip/breakpoint/springcloud/EurekaClientProvider8001.java
+++ spring-cloud-eureka-client-provider-8001/src/main/java/vip/breakpoint/springcloud/EurekaClientProvider8001.java
@@ -3,7 +3,7 @@ package vip.breakpoint.springcloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author :breakpoint/赵立刚
@@ -11,7 +11,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
  */
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableEurekaClient
+//@EnableEurekaClient
 public class EurekaClientProvider8001 {
 
     public static void main(String[] args) {
