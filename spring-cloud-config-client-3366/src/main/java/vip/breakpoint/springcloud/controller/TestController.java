81aa2cbb388101edd4a06de93b87f5e4f7d483d6
赵立刚 <zlgtop@163.com>
2020-07-30 07:19:23 +0800

    Spring Cloud bus for rabbitmq 的测试代码小结

---
+++ spring-cloud-config-client-3366/src/main/java/vip/breakpoint/springcloud/controller/TestController.java
@@ -0,0 +1,25 @@
+package vip.breakpoint.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author :breakpoint/赵立刚
+ * @date : 2020/07/29
+ */
+@RefreshScope
+@RestController
+public class TestController {
+
+    @Value("${config.info}")
+    private String configInfo;
+
+
+    @GetMapping("/getConfigInfo")
+    public String getConfigInfo() {
+        return "from config client,config info is" + configInfo;
+    }
+
+}
