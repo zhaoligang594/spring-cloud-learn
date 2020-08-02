ac28a6ab057d862a82875fafe3e4c2d8e9560acf
赵立刚 <zlgtop@163.com>
2020-08-02 09:21:02 +0800

    sentinel 的测试代码小结

--- spring-cloud-alibaba-provider8001/src/main/java/vip/breakpoint/springcloud/controller/NacosController.java
+++ spring-cloud-alibaba-provider8001/src/main/java/vip/breakpoint/springcloud/controller/NacosController.java
@@ -1,5 +1,7 @@
 package vip.breakpoint.springcloud.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
@@ -18,9 +20,16 @@ public class NacosController {
     @Value("${config.info}")
     private String configInfo;
 
+    @SentinelResource(value = "getConfigInfo", blockHandler = "my_blockHandler")
     @GetMapping("/getConfigInfo")
     public String getConfigInfo() {
         return configInfo;
+    }
+
+    // 限流返回的方法
+    public String my_blockHandler(BlockException blockException) {
+
+        return "你好，我是自定义的返回    "+ configInfo;
     }
 
 }
